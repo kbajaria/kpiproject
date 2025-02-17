20d36a61755a2708590f19ed180dda9a83b55b09
kbajaria <kbajaria@gmail.com>
2025-02-17 17:52:13 +0000

    Create flatten_json.sql

---
+++ flatten_json.sql
@@ -0,0 +1,17 @@
+WITH json_data AS (
+    SELECT
+        RECORD_CONTENT
+    FROM
+        SAMPLE_DATA_ORDERS
+)
+
+SELECT
+    json_data.RECORD_CONTENT:address.city::string AS city,
+    json_data.RECORD_CONTENT:address.state::string AS state,
+    json_data.RECORD_CONTENT:address.zipcode::string AS zipcode,
+    json_data.RECORD_CONTENT:itemid::string AS itemid,
+    json_data.RECORD_CONTENT:orderid::string AS orderid,
+    json_data.RECORD_CONTENT:ordertime::string AS ordertime
+    
+FROM
+    json_data
